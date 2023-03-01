836010bd90f27819454879056e1ad92220897854
Anish Prabhu <anishprabhu1997@gmail.com>
2023-03-01 11:18:20 -0600

    updated site

--- src/components/footer.js
+++ src/components/footer.js
@@ -72,19 +72,19 @@ const Footer = () => (
     <StyledSocialLinks>
       <ul>
         {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <a href={url} aria-label={name}>
-                  <Icon name={name} />
-                </a>
-              </li>
-            ))}
+          socialMedia.map(({ name, url }, i) => (
+            <li key={i}>
+              <a href={url} aria-label={name}>
+                <Icon name={name} />
+              </a>
+            </li>
+          ))}
       </ul>
     </StyledSocialLinks>
 
     <StyledCredit tabindex="-1">
       <a href="https://github.com/APrabhu97/v4">
-        <div>Designed by Brittany Chiang and built by me</div>
+        <div>By Anish Prabhu</div>
       </a>
     </StyledCredit>
   </StyledFooter>
